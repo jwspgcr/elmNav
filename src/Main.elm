3ebbeb58877bbe12140bef8e72f885ee5be458aa
jwspgcr <syaga.works@gmail.com>
2021-04-19 02:27:38 +0900

    add elmNav to URL

--- src/Main.elm
+++ src/Main.elm
@@ -5,7 +5,8 @@ import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Url
-
+import Url.Parser as Parser exposing (Parser, (</>), custom, fragment, map, oneOf, s, top)
+import Debug
 
 
 -- MAIN
@@ -30,12 +31,13 @@ main =
 type alias Model =
   { key : Nav.Key
   , url : Url.Url
+  , extra : String
   }
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-  ( Model key url, Cmd.none )
+  ( Model key url "initial", Cmd.none )
 
 
 
@@ -59,9 +61,51 @@ update msg model =
           ( model, Nav.load href )
 
     UrlChanged url ->
+      stepUrl url model
+
+-- ROUTER
+
+stepUrl : Url.Url -> Model -> (Model, Cmd Msg)
+stepUrl url model =
+  let
+    parser =
+      oneOf
+        [ route (Parser.s "elmNav")
+            ( { model | url = url, extra = "This is top."}, Cmd.none
+            )
+        , route (Parser.s "elmNav/home")
+        ( { model | url = url, extra = "home."}, Cmd.none
+        )
+        , route (Parser.s "elmNav/reviews" </> Parser.string)
+        ( \bookName -> ({ model | url = url, extra = bookName}, Cmd.none)
+        )
+        , route (Parser.s "elmNav/add" </> Parser.int </> Parser.int)
+        ( \arg1 arg2 ->
+          ({ model | url = url
+                   , extra = String.fromInt (Debug.log "arg" (arg1 + arg2))}
+          , Cmd.none)
+        )
+        -- , route (s "help")
+        --     ({ model | page=Hello })
+        --     -- (stepHelp model (Help.init session "Design Guidelines" "/assets/help/design-guidelines.md"))
+        -- , route (s "help" </> s "documentation-format")
+        --     (stepHelp model (Help.init session "Documentation Format" "/assets/help/documentation-format.md"))
+        ]
+  in
+  case Parser.parse parser url of
+    Just answer ->
+      answer
+
+    Nothing ->
       ( { model | url = url }
       , Cmd.none
       )
+
+
+route : Parser a b -> a -> Parser (b -> c) c
+route parser handler =
+  Parser.map handler parser
+
 
 
 
@@ -89,7 +133,9 @@ view model =
           , viewLink "/reviews/the-century-of-the-self"
           , viewLink "/reviews/public-opinion"
           , viewLink "/reviews/shah-of-shahs"
+          , viewLink "/add/11/10"
           ]
+      , text model.extra
       ]
   }
 
